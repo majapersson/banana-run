ed93401ff90aba09e98763cd6a7798835c1bec5c
linntunberg <linn.tunberg@gmail.com>
2018-11-08 14:24:37 +0100

    arduino file

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,12 +1,21 @@
-int input = 2;
-int state = 0;
+int banana1 = 2;
+int banana2 = 4;
+int state1 = 0;
+int state2 = 0;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  state = digitalRead(input);
-  Serial.println(state);
+  state1 = digitalRead(banana1);
+  state2 = digitalRead(banana2);
+
+  if(state1 == 0) {
+    Serial.println("0");
+  } 
+    if(state2 == 0) {
+    Serial.println("1");
+  } 
   delay(20);
 }
